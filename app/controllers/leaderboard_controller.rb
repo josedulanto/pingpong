94699a7289cb8f627700de7c79c49456176acdfb
José Dulanto <jose.dulanto@live.com>
2015-10-09 13:46:10 -0500

    removes limit for pagination

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,14 +1,11 @@
 class LeaderboardController < ApplicationController
   
-  before_filter :set_leaderboard
-  
   def show
-    limit = params.fetch(:limit, 25).to_i
     page = params.fetch(:page, 1).to_i
     @users = @leaderboard.leaders(page, {with_member_data: true})
     
     pager = Kaminari.paginate_array(@users, total_count: @leaderboard.total_members)
-    @page_array = pager.page(page).per(limit)
+    @page_array = pager.page(page)
   end
   
 end
