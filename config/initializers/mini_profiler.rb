c8d4cddccec5095bc691536b28a14a7ced532be9
José Dulanto <jose.dulanto@live.com>
2015-10-08 19:29:37 -0500

    makes Redis.current work with RedisToGo

--- config/initializers/mini_profiler.rb
+++ config/initializers/mini_profiler.rb
@@ -9,3 +9,5 @@ uri = URI.parse(ENV["REDIS_SERVER_URL"])
 Rack::MiniProfiler.config.storage_options = { :host => uri.host, :port => uri.port, :password => uri.password }
 Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
 Rack::MiniProfiler.config.start_hidden = true
+
+Redis.current = Redis.new(url: uri)
