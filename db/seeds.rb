43258c63157e4c157a1391e4544c7cd33175723a
José Dulanto <jose.dulanto@live.com>
2015-10-09 15:18:20 -0500

    adds gravatar_url to users and deletes the current leaderboard to start fresh

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 require 'services/pingpong_board'
 # Create users
 leaderboard = PingPongBoard.default
+leaderboard.delete_leaderboard
 
 50.times do
   user = User.create(email: Faker::Internet.email, password: "password", password_confirmation: "password", confirmed_at: Time.now)
   puts "==> User #{user.email} created successfully"
-  leaderboard.rank_member(user.id, 0, { email: user.email })
+  leaderboard.rank_member(user.id, 0, { email: user.email, gravatar_url: user.gravatar_url })
   puts "==> User #{user.email} added to leaderboard"
 end
